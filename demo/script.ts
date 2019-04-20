c77257fb8d338bd4b62b80c84ace33dfc0505f3f
dimarodnikov <dimarodnikov@yandex-team.ru>
2019-04-20 23:19:41 +0300

    Add input to DOM inside script.ts

--- demo/script.ts
+++ demo/script.ts
@@ -1,22 +1,27 @@
 import {ScreenShotCreator} from '../src/ScreenShotCreator';
 
-const videoInput = document.getElementById('videoInput') as HTMLInputElement;
+document.addEventListener('DOMContentLoaded', () => {
+    const videoInput = document.createElement('input');
+    videoInput.type = 'file';
+    videoInput.accept = 'video/*';
+    document.body.appendChild(videoInput);
 
-const renderThumbnails = (thumbnails: string[]): void => {
-    thumbnails.forEach(thumbnail => {
-        const img = document.createElement('img');
-        img.src = thumbnail;
-        img.style.width = '50px';
-        document.body.appendChild(img);
-    });
-};
+    const renderThumbnails = (thumbnails: string[]): void => {
+        thumbnails.forEach(thumbnail => {
+            const img = document.createElement('img');
+            img.src = thumbnail;
+            img.style.width = '50px';
+            document.body.appendChild(img);
+        });
+    };
 
-const handleVideoInputChange = async (): Promise<void> => {
-    const file = videoInput.files[0];
-    const fileUrl = window.URL.createObjectURL(file);
-    const screenShotCreator = new ScreenShotCreator(fileUrl);
-    const thumbnails = await screenShotCreator.getScreenShots({numberOfThumbnails: 10});
-    renderThumbnails(thumbnails);
-};
+    const handleVideoInputChange = async (): Promise<void> => {
+        const file = videoInput.files[0];
+        const fileUrl = window.URL.createObjectURL(file);
+        const screenShotCreator = new ScreenShotCreator(fileUrl);
+        const thumbnails = await screenShotCreator.getScreenShots({numberOfThumbnails: 10});
+        renderThumbnails(thumbnails);
+    };
 
-videoInput.addEventListener('change', handleVideoInputChange);
+    videoInput.addEventListener('change', handleVideoInputChange);
+});
